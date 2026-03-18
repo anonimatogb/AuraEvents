28b21ba8cd56990af19fd59180483e9258b3b405
anonimatogb <gabrielcavalcante22@outlook.com>
2026-03-18 15:40:48 -0300

    terminar em casa

--- controller/eventoscontroller.php
+++ controller/eventoscontroller.php
@@ -4,22 +4,33 @@ require_once "../Model/EventosModel.php";
 class EventosController
 {
     private $EventosModel;
-    private $pdo; // ✅ guardar o PDO aqui também
-
+private $pdo;   
     public function __construct($pdo)
     {
-        $this->pdo = $pdo; // ✅ salva para usar depois
-        $this->EventosModel = new EventosModel($pdo);
+
+    $this->EventosModel = new EventosModel($pdo);
+    $this->pdo = $pdo;
+
     }
 
-    public function alistar()
+     public function buscareventos($id)
+    {
+        $eventos = $this->EventosModel->buscareventos($id);
+        return $eventos;
+    }
+ public function listar()
+    {
+        $eventos = $this->EventosModel->buscarTodos();
+        include_once "../listagem/listarevent.php";
+        return $eventos;
+    }
+  public function alistar()
     {
         $eventos = $this->EventosModel->buscarTodos();
         $pdo = $this->pdo; // ✅ agora existe
         include_once __DIR__ . "/../listagem/nada.php";
         return $eventos;
     }
-
     public function cadastrarevento($nome, $descricao, $data_evento, $horario, $local_evento, $max_participantes)
     {
         return $this->EventosModel->cadastrarevento($nome, $descricao, $data_evento, $horario, $local_evento, $max_participantes);
